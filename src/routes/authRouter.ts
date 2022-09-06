e234ebd3283910f11e71593fd5c8fe47f99906af
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-06 15:12:46 -0300

    :sparkles: feat: remake signup schema

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,6 +1,11 @@
 import { Router } from "express"; 
+import schemaValidate from "../middlewares/schemaValidate.js";
+
+import authSchemas from "../schemas/authSchema.js";
+
 
 const authRouter = Router();
-console.log("teste")
+
+authRouter.post("/signup", schemaValidate(authSchemas.signupSchema));
 
 export default authRouter
---
+++ src/schemas/authSchema.ts
@@ -0,0 +1,24 @@
+import Joi from 'joi';
+
+const signupSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required().pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/),
+});
+
+const authSchemas = {
+  signupSchema,
+}
+
+export default authSchemas
+
+
+// TODO : document this
+/**
+/^
+  (?=.*\d)              // deve conter ao menos um dígito
+  (?=.*[a-z])           // deve conter ao menos uma letra minúscula
+  (?=.*[A-Z])           // deve conter ao menos uma letra maiúscula
+  (?=.*[$*&@#])         // deve conter ao menos um caractere especial
+  [0-9a-zA-Z$*&@#]{10,}  // deve conter ao menos 10 dos caracteres mencionados
+$/
+ */
