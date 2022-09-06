d83432f62ff24b3dd73fc0de5ffdd9e5af2a2c8a
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-06 14:20:18 -0300

    :heavy_plus_sign: build: added prettier and eslint settings

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,28 +1,31 @@
-import pg from 'pg';
-import dotenv from 'dotenv';
-import "./setup.js"
-dotenv.config();
+import pg from 'pg'
+import dotenv from 'dotenv'
+import './setup.ts'
 
-const { Pool } = pg;
+dotenv.config()
+
+const { Pool } = pg
 
 const devConfig = {
-  host: "127.0.0.1",
+  host: '127.0.0.1',
   port: 5432,
   user: process.env.POSTGRES_USERNAME,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
 }
 
-const prodConfig:any = {
-  connectionString: process.env.DATABASE_URL
+const prodConfig: any = {
+  connectionString: process.env.DATABASE_URL,
 }
 
-if(process.env.MODE === "PROD"){
+if (process.env.MODE === 'PROD') {
   prodConfig.ssl = {
     rejectUnauthorized: false,
   }
 }
 
-const connection = new Pool(process.env.MODE === "PROD" ? prodConfig : devConfig);
+const connection = new Pool(
+  process.env.MODE === 'PROD' ? prodConfig : devConfig,
+)
 
-export default connection;
+export default connection
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
-import chalk from 'chalk';
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/extensions */
+import chalk from 'chalk'
 
-import app from './app.js';
+// eslint-disable-next-line import/no-unresolved
+import app from './app.js'
 
-const port = +process.env.PORT || 9000;
+const port = +process.env.PORT || 9000
 
 app.listen(port, () => {
-  console.log("")
+  console.log('')
   console.log(chalk.green.bold(`Server is up and running on port ${port}`))
-  console.log(chalk.yellow.bold(`Mode: ${process.env.MODE || "not defined -> DEV"}`))
-  console.log(chalk.yellow(`Verbose: ${process.env.VERBOSE || "false"}`))
-  console.log("---------------------------------------")
+  console.log(
+    chalk.yellow.bold(`Mode: ${process.env.MODE || 'not defined -> DEV'}`),
+  )
+  console.log(chalk.yellow(`Verbose: ${process.env.VERBOSE || 'false'}`))
+  console.log('---------------------------------------')
 })
