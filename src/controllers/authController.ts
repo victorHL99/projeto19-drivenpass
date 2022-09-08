bd8a6eb31a746b52eda4c8f7280121ec7eba2ba4
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-08 15:16:04 -0300

    :heavy_plus_sign: build: added dependence bcrypt

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,14 +4,16 @@ import { Request, Response } from 'express';
 import authService from '../services/authService.js'
 
 // import interfaces
-import { CreateAuthUser } from '../interfaces/authInterface.js';
+import { CreateAuthUser } from '../types/authInterface.js';
 
-async function createUser(req:Request, res:Response){
-  const { email, password }:CreateAuthUser = req.body;
+async function createUser(req: Request, res: Response) {
+  const { email, password }: CreateAuthUser = req.body;
   console.log(email, password);
 
-// verify if email already exists
+  // verify if email already exists
   await authService.verifyEmailExists(email);
+
+
 
 
   res.sendStatus(201);
