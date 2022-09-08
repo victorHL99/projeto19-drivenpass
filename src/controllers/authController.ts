8b7dcb1021a103dc02ede4e40c8c7cc51f10a1c2
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-08 15:37:01 -0300

    :bug: fix: bug fixed for types at hashPassword

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { users } from '@prisma/client';
 
 // import services
 import authService from '../services/authService.js'
@@ -8,13 +9,10 @@ import { CreateAuthUser } from '../types/authInterface.js';
 
 async function createUser(req: Request, res: Response) {
   const { email, password }: CreateAuthUser = req.body;
-  console.log(email, password);
-
 
   await authService.verifyEmailExists(email); // verify if email already exists
-  const hashPassword = await authService.encryptPassword(password); // encrypt password 
+  const hashPassword: users['password'] = await authService.encryptPassword(password); // encrypt password 
   await authService.createUserDatabase(email, hashPassword);// create user
-
 
   res.status(201).send("User created");
 }
