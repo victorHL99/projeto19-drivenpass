868468ddddbbaec181289f9a85b89ffdc0cb2e0f
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-08 14:58:01 -0300

    :sparkles: feat: modified query for find email

--- src/repositories/authRepository.ts
+++ src/repositories/authRepository.ts
@@ -1,9 +1,13 @@
-import connection from "../config/database.js";
+import client from "../config/database.js";
 import { IAuthUser } from "../interfaces/authInterface.js";
 
 
 async function getEmail(email: IAuthUser['email']) {
-  return connection.query<IAuthUser>(`SELECT * FROM users WHERE email = $1`, [email]);
+  return client.users.findUnique({
+    where: {
+      email
+    }
+  });
 }
 
 const authRepository = {
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,9 +3,9 @@ import { IAuthUser } from "../interfaces/authInterface";
 // import repositories
 import authRepository from "../repositories/authRepository.js";
 
-async function verifyEmailExists(email:IAuthUser['email']) {
+async function verifyEmailExists(email: IAuthUser['email']) {
   const resultEmail = await authRepository.getEmail(email);
-  if(resultEmail.rows.length > 0){
+  if (resultEmail) {
     throw {
       type: 'conflict',
       message: 'Email already exists',
