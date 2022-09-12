0010b25f526a5528504092b3dd3dff0c7b1fdbab
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-12 11:02:12 -0300

    :sparkles: feat: create new migration on prisma

---
+++ prisma/migrations/20220912140051_create_table_for_notes/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "notes" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "notes_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "notes" ADD CONSTRAINT "notes_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
